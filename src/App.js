d7f063064d3563a1ba4b02da049feb56a5dddc76
b0ndurant <martin.aurelien01@gmail.com>
2019-05-17 12:24:03 +0200

    create react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Home from "./Home.js";
+import History from "./History";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="topnav">
+          <NavLink exact to="/">Home</NavLink>
+          <NavLink to="/notre-histoire">Notre Histoire</NavLink>
+        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/notre-histoire" component={History} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() { 
+        return ( <h1>Bienvenue sur l'accueil</h1>  );
+    }
+}
+ 
+export default Home;
